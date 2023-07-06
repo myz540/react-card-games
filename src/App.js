eedd9b291e6c82c8034798b0a21512ed5d6d079a
Mike Zhong <mzhong@tensixteen.bio>
2023-07-06 15:44:09 -0400

    lots of basic components made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { CARD_MAP, shuffle } from "./constants";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [cards, setCards] = useState(CARD_MAP.slice());
+  const [hand, setHand] = useState([]);
+  const topCard = cards[0];
+  const bottomCard = cards[-1];
+
+  function handleDrawCard() {
+    console.log(topCard);
+    setHand((h) => [...h, topCard]);
+    setCards((c) => c.filter((i) => i.name !== topCard.name));
+  }
+
+  function handleShuffle() {
+    console.log("Shuffling");
+    setCards(shuffle(CARD_MAP.slice()));
+    setHand([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Deck
+        cards={cards}
+        onDrawCard={handleDrawCard}
+        onShuffle={handleShuffle}
+      />
+      <Hand cards={hand} />
     </div>
   );
 }
 
-export default App;
+function Card({ card }) {
+  return <div className={`card ${card.suit}`}>{card.icon}</div>;
+}
+
+function Deck({ cards, onDrawCard, onShuffle }) {
+  const cardsInDeck = cards.length;
+  return (
+    <div className="deck">
+      <h3 onClick={onDrawCard}>🎴</h3>
+      <p>Cards Left: {cardsInDeck}</p>
+      <button onClick={onShuffle}>Shuffle</button>
+    </div>
+  );
+}
+
+function Hand({ cards }) {
+  return (
+    <div className="hand">
+      {cards.map((c) => (
+        <Card card={c} />
+      ))}
+    </div>
+  );
+}
---
+++ src/constants.js
@@ -0,0 +1,85 @@
+export const _CARD_MAP = Object.freeze({
+  // ACE
+  SPADE_A: "🂡",
+  CLUB_A: "🃑",
+  HEART_A: "🂱",
+  DIAMOND_A: "🃁",
+  // TWO
+  SPADE_2: "🂢",
+  CLUB_2: "🃒",
+  HEART_2: "🂲",
+  DIAMOND_2: "🃂",
+  // THREE
+  SPADE_3: "🂣",
+  CLUB_3: "🃓",
+  HEART_3: "🂳",
+  DIAMOND_3: "🃃",
+  // FOUR
+  SPADE_4: "🂤",
+  CLUB_4: "🃔",
+  HEART_4: "🂴",
+  DIAMOND_4: "🃄",
+  // FIVE
+  SPADE_5: "🂥",
+  CLUB_5: "🃕",
+  HEART_5: "🂵",
+  DIAMOND_5: "🃅",
+  // SIX
+  SPADE_6: "🂦",
+  CLUB_6: "🃖",
+  HEART_6: "🂶",
+  DIAMOND_6: "🃆",
+  // SEVEN
+  SPADE_7: "🂧",
+  CLUB_7: "🃗",
+  HEART_7: "🂷",
+  DIAMOND_7: "🃇",
+  // EIGHT
+  SPADE_8: "",
+  CLUB_8: "",
+  HEART_8: "",
+  DIAMOND_8: "",
+  // NINE
+  SPADE_9: "",
+  CLUB_9: "",
+  HEART_9: "",
+  DIAMOND_9: "",
+  // TEN
+  SPADE_10: "",
+  CLUB_10: "",
+  HEART_10: "",
+  DIAMOND_10: "",
+  // JACK
+  SPADE_11: "",
+  CLUB_11: "",
+  HEART_11: "",
+  DIAMOND_11: "",
+  // QUEEN
+  SPADE_12: "🂡",
+  CLUB_12: "🃑",
+  HEART_12: "🂱",
+  DIAMOND_12: "🃁",
+  // KING
+  SPADE_13: "🂡",
+  CLUB_13: "🃑",
+  HEART_13: "🂱",
+  DIAMOND_13: "🃁",
+});
+
+export const CARD_MAP = Object.entries(_CARD_MAP).map((card) => ({
+  name: card[0],
+  icon: card[1],
+  suit: card[0].split("_")[0],
+  value: card[0].split("_")[1],
+}));
+
+export function shuffle(array) {
+  const newArray = array.slice();
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = newArray[i];
+    newArray[i] = newArray[j];
+    newArray[j] = temp;
+  }
+  return newArray;
+}
