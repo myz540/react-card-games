fbcfd9ab543431eb4a8061605f760a57ebc93a3a
Mike Zhong <mike.zhong@precede.bio>
2024-10-13 14:58:43 -0400

    working game

--- src/Solitaire.js
+++ src/Solitaire.js
@@ -1,5 +1,5 @@
 import { CARD_MAP, shuffle, CARD_BACK } from "./constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Deck from "./Deck";
 import Card from "./Card";
 import Foundation from "./Foundation";
@@ -15,18 +15,44 @@ export default function App() {
     CLUB: [],
   });
 
+  const [isGameWon, setIsGameWon] = useState(false);
+
+  useEffect(() => {
+    checkWinCondition();
+  }, [foundations, cards]);
+
+  function checkWinCondition() {
+    const allFoundationsFull = Object.values(foundations).every(
+      (foundation) => foundation.length === 13
+    );
+    const deckEmpty = cards.length === 0 && !shownCard;
+
+    if (allFoundationsFull && deckEmpty) {
+      setIsGameWon(true);
+    }
+  }
+
   function handleDrawCard() {
-    const tempCards = cards.slice();
-    const card = tempCards.shift();
-    console.log(card);
-    setCards(() => [...tempCards, card]);
-    setShownCard(card);
+    if (cards.length > 0) {
+      const drawnCard = cards[0];
+      setShownCard(drawnCard);
+      setCards(cards.slice(1));
+    } else {
+      setShownCard(null);
+    }
   }
 
   function handleShuffle() {
-    console.log("Shuffling");
     setCards(shuffle(CARD_MAP.slice()));
     setShownCard(null);
+    setSelectedCard(null);
+    setFoundations({
+      DIAMOND: [],
+      SPADE: [],
+      HEART: [],
+      CLUB: [],
+    });
+    setIsGameWon(false);
   }
 
   function handleCardClick(card) {
@@ -43,10 +69,16 @@ export default function App() {
         ...prev,
         [suit]: [...prev[suit], selectedCard],
       }));
+
       if (selectedCard === shownCard) {
-        setShownCard(null);
+        // If the placed card was the shown card, draw a new card
+        handleDrawCard();
+      } else {
+        // If the placed card was from elsewhere (e.g., tableau in future implementations),
+        // we don't need to draw a new card
+        setCards((prev) => prev.filter((c) => c.name !== selectedCard.name));
       }
-      setCards((prev) => prev.filter((c) => c.name !== selectedCard.name));
+
       setSelectedCard(null);
     }
   }
@@ -61,7 +93,25 @@ export default function App() {
     }
 
     const topCard = foundationPile[foundationPile.length - 1];
-    return parseInt(card.value) === parseInt(topCard.value) + 1;
+    const cardValues = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    const topCardIndex = cardValues.indexOf(topCard.value);
+    const currentCardIndex = cardValues.indexOf(card.value);
+
+    return currentCardIndex === topCardIndex + 1;
   }
 
   return (
