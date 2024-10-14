c49e659ec1286ee175db03bb2faef842d8c17fd8
Mike Zhong <mike.zhong@precede.bio>
2024-10-14 10:46:06 -0400

    style win banner

---
+++ src/WinBanner.js
@@ -0,0 +1,11 @@
+const WinBanner = ({ onPlayAgain }) => {
+  return (
+    <div className="win-banner-overlay">
+      <div className="win-banner">
+        <h2>Congratulations!</h2>
+        <p>You've won the game!</p>
+        <button onClick={onPlayAgain}>Play Again</button>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./Solitaire.css";
 // import App from "./App";
 import App from "./Solitaire";
 
