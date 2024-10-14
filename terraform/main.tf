c5cdc170fb853d5584790dfb3538505ecec96be5
Mike Zhong <mike.zhong@precede.bio>
2024-10-14 13:45:37 -0400

    add tf

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+provider "aws" {
+  region = "us-west-2" # or your preferred region
+}
+
+resource "aws_dynamodb_table" "users" {
+  name         = "Users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "EmailIndex"
+    hash_key        = "email"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name = "Users Table"
+  }
+}
+
+resource "aws_dynamodb_table" "games" {
+  name         = "Games"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Games Table"
+  }
+}
+
+resource "aws_dynamodb_table" "game_states" {
+  name         = "GameStates"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Game States Table"
+  }
+}
+
+resource "aws_dynamodb_table" "players" {
+  name         = "Players"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "UserIdIndex"
+    hash_key        = "userId"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name = "Players Table"
+  }
+}
