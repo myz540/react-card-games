b0ee62faeea8b49d53f6053f9ef9fb62251a6f83
Mike Zhong <mike.zhong89@gmail.com>
2024-10-26 12:27:30 -0400

    looks nice

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -5,27 +5,30 @@ import "./Hand.css";
 
 const Hand = ({ player, isSelf, orientation, onCardPlay, isVertical }) => {
   return (
-    <div
-      className={`hand ${orientation} ${isSelf ? "self" : "other"} ${
-        isVertical ? "vertical" : ""
-      }`}
-    >
-      {player.hand.map((card, index) => (
-        <div
-          key={card.name}
-          className={`hand-card ${
-            index === player.hand.length - 1 ? "top" : "stacked"
-          }`}
-          style={{
-            zIndex: index + 1,
-            [isVertical ? "top" : "left"]: `${index * 20}px`,
-          }}
-          onClick={() => isSelf && onCardPlay(player.id, card)}
-        >
-          <Card card={isSelf ? card : CARD_BACK} faceUp={isSelf} />
+    <div className={`hand ${orientation} ${isSelf ? "self" : "other"} ${isVertical ? "vertical" : ""}`}>
+      <div className="hand-cards">
+        {player.hand.map((card, index) => (
+          <div
+            key={card.name}
+            className={`hand-card ${
+              index === player.hand.length - 1 ? "top" : "stacked"
+            }`}
+            style={{
+              zIndex: index + 1,
+              [isVertical ? 'top' : 'left']: `${index * 20}px`,
+            }}
+            onClick={() => isSelf && onCardPlay(player.id, card)}
+          >
+            <Card card={isSelf ? card : CARD_BACK} faceUp={isSelf} />
+          </div>
+        ))}
+      </div>
+      <div className="player-info-wrapper">
+        <div className="player-info">
+          <div className="player-name">{player.user ? player.user.username : `Player ${player.id}`}</div>
+          <div className="card-count">{player.hand.length} cards</div>
         </div>
-      ))}
-      <div className="player-name">{player.name}</div>
+      </div>
     </div>
   );
 };
